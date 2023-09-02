9d63b19f9a7bc2ef153fe9c5bfeb4eca837faa81
odi1n <sashf1999@mail.ru>
2023-09-03 02:16:36 +0300

    feat(user): password hash and verify

--- src/user/services.py
+++ src/user/services.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+import bcrypt
+
 from src.common.mixins.services import ServiceRepositoryMixin
 from src.user import schemas as sc
 from src.user.interfaces import UserRepositoryInterface
@@ -15,3 +17,13 @@ class UserService(ServiceRepositoryMixin[UserRepositoryInterface, sc.UserView]):
         if user and self.password_verify(password, user.password):
             return user
         return None
+
+    @staticmethod
+    def get_password_hash(plain_password: str) -> str:
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(plain_password.encode(), salt).decode()
+        return hashed
+
+    @staticmethod
+    def password_verify(plain_password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
