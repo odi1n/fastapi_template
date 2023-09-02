1da47f0a1c70cb2503d77666741e6598fe392af5
odi1n <sashf1999@mail.ru>
2023-09-03 02:15:26 +0300

    feat(container): set config

--- src/containers.py
+++ src/containers.py
@@ -6,9 +6,10 @@ from src.user.services import UserService
 
 
 class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(packages=["src"], auto_wire=True)
     config = providers.Configuration()
 
-    db = providers.Singleton(Database)
+    db = providers.Singleton(Database, db_url=config.DB_DSN)
 
     user_repository = providers.Factory(UserRepository, session=db.provided.get_session)
     user_service = providers.Factory(UserService, repository=user_repository)
