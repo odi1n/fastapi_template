d2629d587e4eea6a74258dee166b71f7718e9518
odi1n <sashf1999@mail.ru>
2023-09-02 16:25:26 +0300

    fix(alembic): remove type ignore

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from alembic import context  # type: ignore
+from alembic import context
 from alembic.script import ScriptDirectory
 from src.database import BaseModel
 from src.settings import setting
