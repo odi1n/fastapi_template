318fe542c044d3fd7ea4558db85557a531de9b23
odi1n <sashf1999@mail.ru>
2023-09-04 00:57:56 +0300

    feat(auth): add

--- src/user/auth.py
+++ src/user/auth.py
@@ -1,24 +1,24 @@
 from datetime import datetime, timedelta
 
+from dependency_injector.wiring import Provide, inject
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-from jose.constants import ALGORITHMS
 
 from src.settings import setting
+from src.user import schemas as sc
+from src.user.private.repositories import UserRepository
 
-from .private.repositories import UserRepository
-from .schemas import UserUnprotectedView
-
-token_scheme = OAuth2PasswordBearer(tokenUrl="authorize", scheme_name="email")
+token_scheme = OAuth2PasswordBearer(tokenUrl="/signin")
 
 
 class Auth:
+    @inject
     async def __call__(
         self,
         token: str = Depends(token_scheme),
-        repository: UserRepository = Depends(),
-    ) -> UserUnprotectedView:
+        repository: UserRepository = Depends(Provide["repositories.user_repository"]),
+    ) -> sc.UserUnprotectedView:
         credentials_exception = HTTPException(
             status_code=401,
             detail="Could not validate credentials",
@@ -26,7 +26,11 @@ class Auth:
         )
 
         try:
-            payload = jwt.decode(token, setting.JWT_SECRET_KEY)
+            payload = jwt.decode(
+                token=token,
+                key=setting.JWT_SECRET_KEY,
+                algorithms="HS256",
+            )
             email = payload.get("sub")
             if email is None:
                 raise credentials_exception from None
@@ -40,16 +44,33 @@ class Auth:
     @staticmethod
     def create_access_token(data: dict) -> str:
         to_encode = data.copy()
-        if setting.JWT_TOKEN_EXPIRE_MINUTES:
+        if setting.JWT_ACCESS_TOKEN_EXPIRE_MINUTES:
             to_encode.update(
                 {
                     "exp": datetime.utcnow()
-                    + timedelta(minutes=setting.JWT_TOKEN_EXPIRE_MINUTES),
+                    + timedelta(minutes=setting.JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
                 },
             )
         token = jwt.encode(
             to_encode,
             setting.JWT_SECRET_KEY,
-            algorithm=ALGORITHMS.HS256,
+            algorithm="HS256",
+        )
+        return token
+
+    @staticmethod
+    def create_refresh_token(data: dict) -> str:
+        to_encode = data.copy()
+        if setting.JWT_REFRESH_TOKEN_EXPIRE_MINUTES:
+            to_encode.update(
+                {
+                    "exp": datetime.utcnow()
+                    + timedelta(minutes=setting.JWT_REFRESH_TOKEN_EXPIRE_MINUTES),
+                },
+            )
+        token = jwt.encode(
+            to_encode,
+            setting.JWT_REFRESH_SECRET_KEY,
+            algorithm="HS256",
         )
         return token
